bdd7dd527fc19f7c442ab609451e24767a8cbf0d
Gabriela T <gtrujillo15@gmail.com>
2019-10-08 13:46:39 -0700

    end of day11

---
+++ arrays.php
@@ -0,0 +1,11 @@
+<?php
+$myIndexArray = array("Katie", "Em", "Molly");
+
+$myAssocArray = array(
+    "name" => "Katie",
+    "Latinx" => True,
+    "Cohort" => "design"
+);
+
+$myOtherAssocArray = array();
+$myOtherAssocArray["name"] = "Nikki";
